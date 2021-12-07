06233ff52bc5fcc6316b9d00e2719ad5b1be5062
Animesh Kumar Shukla <56479786+animeshaks@users.noreply.github.com>
2021-12-07 22:30:23 +0530

    Updates

---
+++ precache-manifest.7311c378751d835e20f61b9e87036574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f7ffd05eeeb80cfeb68f5d8f490a15",
+    "url": "/reactjs-tesla-clone/index.html"
+  },
+  {
+    "revision": "56b6ba9f862b1dc3b970",
+    "url": "/reactjs-tesla-clone/static/css/main.c0411cc6.chunk.css"
+  },
+  {
+    "revision": "ae6519e9559a36ca5f7a",
+    "url": "/reactjs-tesla-clone/static/js/2.d026d161.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/reactjs-tesla-clone/static/js/2.d026d161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b6ba9f862b1dc3b970",
+    "url": "/reactjs-tesla-clone/static/js/main.d0185756.chunk.js"
+  },
+  {
+    "revision": "da7029faa8f522186948",
+    "url": "/reactjs-tesla-clone/static/js/runtime-main.e4d8f41d.js"
+  }
+]);
