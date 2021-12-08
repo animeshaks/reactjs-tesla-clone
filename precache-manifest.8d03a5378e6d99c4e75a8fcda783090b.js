a570b57e5a560a9bcb4e800e1bab42d9c90a1e62
Animesh Kumar Shukla <56479786+animeshaks@users.noreply.github.com>
2021-12-08 19:45:50 +0530

    Updates

---
+++ precache-manifest.8d03a5378e6d99c4e75a8fcda783090b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d4f5dd782c0c0d122026bc23056f8f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aecc6dc4639df5d01702",
+    "url": "./static/css/main.c0411cc6.chunk.css"
+  },
+  {
+    "revision": "76878004514efeabbe39",
+    "url": "./static/js/2.f140d018.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "./static/js/2.f140d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecc6dc4639df5d01702",
+    "url": "./static/js/main.37ef3a5b.chunk.js"
+  },
+  {
+    "revision": "c8e354c915018552faab",
+    "url": "./static/js/runtime-main.7c2f0d34.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs-tesla-clone/precache-manifest.7311c378751d835e20f61b9e87036574.js"
+  "./precache-manifest.8d03a5378e6d99c4e75a8fcda783090b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs-tesla-clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
